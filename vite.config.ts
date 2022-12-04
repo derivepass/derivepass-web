ea8faf642eeabf94be352123fc773b73a3eb0236
Fedor Indutny <238531+indutny@users.noreply.github.com>
2022-12-04 13:33:48 -0800

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import wasmPack from 'vite-plugin-wasm-pack';
+import { VitePWA } from 'vite-plugin-pwa';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), wasmPack(['./scrypt-crate'])],
+  plugins: [
+    svelte(),
+    wasmPack(['./scrypt-crate']),
+    VitePWA(),
+  ],
 })
