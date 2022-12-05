963084f68d32d48dc6508ca2cfa157fbae3364e8
Fedor Indutny <238531+indutny@users.noreply.github.com>
2022-12-04 18:54:22 -0800

    Add sourcemaps for curious devs?

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   plugins: [
     svelte(),
     wasmPack(['./scrypt-crate']),
